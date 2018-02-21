8f1fb687a913279260a2c648c6f0cb871f07e96e
Edric Ng <edric@talenta.co>
2018-02-21 14:04:52 +0700

    first commit

---
+++ number.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Number</title>
+    </head>
+    <body>
+        <div>
+            <?php 
+                for ($x = 0; $x <= 10; $x++) {
+                    echo "The number is: $x <br>";
+                } 
+            ?>
+        </div>
+    </body>
+</html>
